76acfda19be38777c533415136927c06ff34ddf3
Andres Lubo <alubo_08@outlook.com>
2022-07-22 10:27:40 -0500

    Separacion de reesponsabilidades del routing

--- index.js
+++ index.js
@@ -1,68 +1,17 @@
 console.log('My-store');
 
 const express = require('express');
-const faker = require('faker');
 const config = require('./config')
+const router = require('./routes/router')
 
 const app = express()
 
 
+router(app)
+
+
 app.get('/', (req, res) => {
     res.send('Hello World!');
-})
-
-app.get('/products', (req, res) => {
-    const products = []
-    const { size } = req.query
-    const limit = size || 10
-
-    for (let index = 0; index < limit; index++) {
-        products.push({
-            name: faker.commerce.productName(),
-            price: parseInt(faker.commerce.price(), 10),
-            image: faker.image.imageUrl()
-        })
-    }
-
-    res.json(products);
-})
-
-app.get('/products/:id', (req, res) => {
-    const { id } = req.params;
-
-    res.json({
-        id,
-        "name": "Product 1",
-        "price": "100"
-    })
-})
-
-app.get('/products/filter', (req, res) => {
-    res.send('Yo soy un filter')
-})
-
-
-app.get('/categories/:categoryId/products/:productId', (req, res) => {
-    const { categoryId, productId } = req.params;
-
-    res.json({
-        categoryId,
-        productId,
-    })
-})
-
-app.get('/users', (req, res) => {
-    const { limit, offset } = req.query
-
-    if (limit && offset) {
-        res.json({
-            limit,
-            offset,
-        })
-    } else {
-        res.send('No hay parámetros query')
-    }
-
 })
 
 app.listen(config.port, () => {
---
+++ routes/router.js
@@ -0,0 +1,12 @@
+// const express = require('express');
+const productsRouter = require('./productsRouter');
+const usersRouter = require('./usersRouter');
+const categoriesRouter = require('./categoriesRouter');
+
+const routerApi = (server) => {
+    server.use('/products', productsRouter);
+    server.use('/users', usersRouter);
+    server.use('/categories', categoriesRouter);
+}
+
+module.exports = routerApi;
