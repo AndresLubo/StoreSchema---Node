7ca35837d38f1177477da24f4edcf2fae782830c
Andres Lubo <alubo_08@outlook.com>
2022-07-22 09:34:04 -0500

    Routing con express

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 console.log('My-store');
+
+const express = require('express');
+const config = require('./config')
+
+const app = express()
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
+
+app.get('/products', (req, res) => {
+    res.json({
+        "name": "Product 1",
+        "price": "100"
+    });
+})
+
+
+app.listen(config.port, () => {
+    console.log(`Server is running on port ${config.port}`);
+})
