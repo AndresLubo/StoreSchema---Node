b1ec375acfa0614a8b93a3bbf8cebd5a6fb3f006
Andres Lubo <alubo_08@outlook.com>
2022-07-22 15:41:40 -0500

    solucion al problema de cors

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const routerApi = require('./routes/router');
 const config = require('./config');
-
 const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler');
+const cors = require('cors')
 
 const app = express();
 
 app.use(express.json());
+
+const whiteList = ['http://localhost:8080', 'https://myapp.co']
+const options = {
+    origin: (origin, callback) => {
+        if (whiteList.includes(origin) || !origin) {
+            callback(null, true)
+        }
+        callback(new Error('no permitido'))
+    }
+}
+app.use(cors(options));
 
 app.get('/', (req, res) => {
     res.send('Hello world');
