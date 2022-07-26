50b2511257e9b570d90ed6b17cf30fd552e6bad1
Andres Lubo <alubo_08@outlook.com>
2022-07-26 16:44:45 -0500

    Segunda migración en la base de datos: añadida la columna rol

--- routes/users.router.js
+++ routes/users.router.js
@@ -3,7 +3,6 @@ const express = require('express');
 const UserService = require('./../services/user.service');
 const validatorHandler = require('./../middlewares/validator.handler');
 const { updateUserSchema, createUserSchema, getUserSchema } = require('./../schemas/user.schema');
-const { queryErrorHandler } = require('./../middlewares/error.handler');
 
 const router = express.Router();
 const service = new UserService();
